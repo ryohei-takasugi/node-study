c92c5fbce78b89edb22d72b3789cbd30941c1656
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2025-05-02 23:07:57 +0900

    [create] add web server

--- json-tool-web/vite.config.ts
+++ json-tool-web/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // リモートアクセスを許可
-    port: 5173      // ポート番号
+    port: 4173      // ポート番号
   }
 })
